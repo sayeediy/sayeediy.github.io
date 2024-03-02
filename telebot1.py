403300cc812ed6956c3550ac0f1ea1eb50dad8e0
sayeediy <153652781+sayeediy@users.noreply.github.com>
2024-03-02 23:04:49 +0500

    Add files via upload

---
+++ telebot1.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
+
+
+bot = Bot('6995587565:AAFTjc42wGPy9GDZy677tBViryb5AmD2Dz8')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(KeyboardButton("Open Website", web_app=WebAppInfo (url='https://github.com/Ali-Sayeed/telegram-web-app1')))
+    await message.answer("Hello, my friend!", reply_markup=markup)
+
+if __name__ == '__main__':
+    from aiogram import executor
+    executor.start_polling(dp)
