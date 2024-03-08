5d80e4a665f7441f34d34c50b3d73c2847a3ae77
sayeediy <153652781+sayeediy@users.noreply.github.com>
2024-03-08 22:45:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,77 +14,48 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+btn1.addEventListener("click", function () {
+	handleButtonClick("1");
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
+btn2.addEventListener("click", function () {
+	handleButtonClick("2");
 });
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
+btn3.addEventListener("click", function () {
+	handleButtonClick("3");
 });
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
+btn4.addEventListener("click", function () {
+	handleButtonClick("4");
 });
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
+btn5.addEventListener("click", function () {
+	handleButtonClick("5");
 });
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
+btn6.addEventListener("click", function () {
+	handleButtonClick("6");
 });
 
+function handleButtonClick(selectedItem) {
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	} else {
+		tg.MainButton.setText(`Вы выбрали товар ${selectedItem}!`);
+		item = selectedItem;
+		tg.MainButton.show();
 
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
+		// Your logic to handle the invoice details here
+		// For example, you can open a payment modal or update the UI
+		// You can use the 'item' variable to get the selected item details
+		console.log("Selected Item:", item);
+	}
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
 	tg.sendData(item);
 });
-
 
 let usercard = document.getElementById("usercard");
 
