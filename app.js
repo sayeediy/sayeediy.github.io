38c00589716642d35374b1af63954e27721016a1
sayeediy <153652781+sayeediy@users.noreply.github.com>
2024-03-08 19:52:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,82 @@
+// ... (existing code)
+
 let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+
+btn1.addEventListener("click", function () {
+    handleButtonClick("1");
+});
+
+btn2.addEventListener("click", function () {
+    handleButtonClick("2");
+});
+
+btn3.addEventListener("click", function () {
+    handleButtonClick("3");
+});
+
+btn4.addEventListener("click", function () {
+    handleButtonClick("4");
+});
+
+btn5.addEventListener("click", function () {
+    handleButtonClick("5");
+});
+
+btn6.addEventListener("click", function () {
+    handleButtonClick("6");
+});
+
+function handleButtonClick(selectedItem) {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText(`Вы выбрали товар ${selectedItem}!`);
+        item = selectedItem;
+        tg.MainButton.show();
+
+        // Your logic to handle the payment process here
+        // For example, you can open a payment modal or update the UI
+        // You can use the 'item' variable to get the selected item details
+        initiatePayment(selectedItem);
+    }
+}
+
+function initiatePayment(selectedItem) {
+    // Your logic to initiate the payment process
+    // This might involve updating the UI, sending a payment link, etc.
+    console.log("Initiating payment for Item:", selectedItem);
+
+    // Example: Redirect to a payment URL
+    window.location.href = 'https://example.com/payment?item=' + selectedItem;
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
+
+/*let tg = window.Telegram.WebApp;
 
 tg.expand();
 
@@ -93,4 +171,4 @@ let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
 
-usercard.appendChild(p);
+usercard.appendChild(p);*/
